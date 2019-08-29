291bf5334823f1002413935b4aace639f4448b0a
Aaron Conway <aaronconwayphd@gmail.com>
2019-08-29 16:26:55 +0000

    prisma diagram

---
+++ zhf_manuscript/prisma_nomnoml.R
@@ -0,0 +1,22 @@
+nomnoml::nomnoml("#import: my-common-styles.nomnoml
+#stroke: black
+#.blob: fill=#DBDBDB visual=italic
+#direction: down
+#edges: rounded
+#arrowSize: 0.6
+#bendSize: 0.3
+#fillArrows: true
+
+  [129 references imported for screening]->[94 studies screened against title and abstract]
+
+  [129 references imported for screening]-->[<blob> 35 duplicates removed]
+
+  [94 studies screened against title and abstract]->[23 studies assessed for full-text eligibility]
+  [94 studies screened against title and abstract]-->[<blob> 71 studies excluded]
+
+[23 studies assessed for full-text eligibility] -> [16 studies included]
+
+ [23 studies assessed for full-text eligibility] --> [<blob> 6 studies excluded | 4 wrong intervention
+                                                      1 wrong comparator
+                                                      1 wrong study design]
+ [23 studies assessed for full-text eligibility] --> [<blob> 1 study awaiting classification]", png = here::here("zhf_manuscript", "figure_one.png"))

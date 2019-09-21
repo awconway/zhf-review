33d064ddcaca2c1ce3ae4e4c09da651e0348ec73
Aaron Conway <aaronconwayphd@gmail.com>
2019-09-21 17:52:42 -0400

    ggplot based prisma works good

---
+++ PRISMAstatement.R
@@ -0,0 +1,6 @@
+# can't get right dpi
+PRISMAstatement::flow_exclusions(
+  incl_counts = c(129, 94, 23, 15),
+  total_label = "References imported for screening",
+  incl_labels = c("Studies screened", "Full-text assessed for eligibility", "Included"),
+  excl_labels = c("Duplicates removed", "Excluded", "Full-text excluded"))

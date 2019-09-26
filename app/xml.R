81200849299a282d5b237a8a6a074794f24e545b
Aaron Conway <aaronconwayphd@gmail.com>
2019-09-26 14:46:54 -0400

    filepath fixed

--- app/xml.R
+++ app/xml.R
@@ -1,7 +1,7 @@
 library(xml2)
 library(tidyverse)
 
-testxml <- xml2::read_xml(here::here("app", "data", "study-characteristics.xml"))
+testxml <- xml2::read_xml(here::here("app", "data", "study-characteristics", "study-characteristics.xml"))
 
 #Study names
 study <- xml_find_all(testxml, ".//ss:Worksheet")
@@ -13,7 +13,7 @@ inclusions <- xml_find_all(rows, "//ss:Row[@ss:Index='17']")
 inclusions <- xml_text(inclusions)
 inclusions <- stringr::str_remove(inclusions, "Inclusion criteria")
 # Dataframe
-datadrame <- tibble::tibble(study, inclusions)
+dataframe <- tibble::tibble(study, inclusions)
 
 #Resource
 #https://stackoverflow.com/questions/42799674/r-and-xml2-how-to-read-text-that-is-not-in-children-nodes-and-read-information
